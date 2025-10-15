e7101638cba9f2a168d0e1b47c5ae690fa293a41
roman-cybersteps <roman@cybersteps.de>
2025-10-15 22:37:14 +0300

    Change password for Wazuh Linux instance for Azure deployment
    Updated the password for the Wazuh Linux instance for the Azure deployment, due to password complexity rules enforced by Azure:
    Error: "admin_password" has to fulfill 3 out of these 4 conditions: Has lower characters, Has upper characters, Has a digit, Has a special character other than "_", fulfilled only 1 conditions

--- extensions/wazuh/providers/azure/linux.tf
+++ extensions/wazuh/providers/azure/linux.tf
@@ -3,6 +3,6 @@
   linux_sku          = "22_04-lts-gen2"
   linux_version      = "latest"
   private_ip_address = "{{ip_range}}.51"
-  password           = "sgdvnkjhdshlsd"
+  password           = "MgrJ/YF$2529"
   size               = "Standard_B2ms"  # 2cpu/8G
 }
